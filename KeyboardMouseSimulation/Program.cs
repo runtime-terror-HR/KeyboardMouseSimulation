1d0141a4944c034eb8b2d11a66714b82900e6ef8
Hiba Alkurd <hibaalkurd@gmail.com>
2021-11-30 21:14:43 +0200

    add mouse support

--- KeyboardMouseSimulation/Program.cs
+++ KeyboardMouseSimulation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO.Ports;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -7,9 +8,6 @@ namespace KeyboardMouseSimulation
 {
     static class Program
     {
-
-        const int A_key = 0x41;
-
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void keybd_event(uint bVk, uint bScan, uint dwFlags, uint dwExtraInfo);
 
@@ -24,23 +22,59 @@ namespace KeyboardMouseSimulation
         [STAThread]
         static void Main()
         {
-            SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
-            port.Open();
+            bool isKeyboard = true;
+            Console.WriteLine("Enter Port (eg. COM3) : ");
+            var portName = Console.ReadLine();
+            SerialPort port = new SerialPort(portName, 9600, Parity.None, 8, StopBits.One);
+            try
+            {
+                port.Open();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("error opening port " + ex.Message);
+                return;
+            }
 
             int count = 0;
             while (true)
             {
                 var rec = port.ReadLine();
+                isKeyboard = rec == "1000" ? !isKeyboard : isKeyboard;
+
                 Console.WriteLine("recieved no. " + count + " : " + rec);
                 count++;
 
-                var valueInInt = uint.Parse(rec);
-                keybd_event(valueInInt, 0, 0, 0);
-
-                //mouse test
-                //Thread.Sleep(5000);
-                //RelativeMove(10, 0);
-
+                
+                if (isKeyboard)
+                {
+                    var keys = rec.Split(',');
+                    foreach (var key in keys)
+                    {
+                        var keyval = key.TrimEnd('\r', '\n');
+                        var tem = (uint)(int) new Int32Converter().ConvertFromString(keyval);
+                        keybd_event(tem, 0, 0, 0);
+                    }
+                    foreach (var key in keys)
+                    {
+                        var keyval = key.TrimEnd('\r', '\n');
+                        var tem = (uint)(int)new Int32Converter().ConvertFromString(keyval);
+                        keybd_event(tem, 0, 0x0002, 0);
+                    }
+                }
+                else
+                {
+                    var keys = rec.Split('\r')[0].Split(',');
+                    try
+                    {
+                        int x = int.Parse(keys[0].TrimEnd('\r', '\n'));
+                        int y = int.Parse(keys[1].TrimEnd('\r', '\n'));
+                        RelativeMove(x, -1 * y);
+                    }
+                    catch
+                    {
+                    }
+                }
             }
         }
     }
